49141bbbaab2119cdf6b5e9e5c1955f0c31d3a2d
ahmetnihat <ahmetnihatrov171@gmail.com>
2025-07-14 11:40:54 +0300

    Add files via upload

--- mission.py
+++ mission.py
@@ -1,4 +1,43 @@
 import turtle
+import logging
+
+def search(start):
+    found = False
+    
+    logger.critical("AREA IS SEARCHING")
+    
+    t.goto(start, target_depth)
+
+    for i in range(2):
+        for j in range(100):
+            t.forward(1)
+            
+            if (t.xcor() >= target_x_min and t.xcor() <= target_x_max) and (t.ycor() >= target_y_min and t.ycor() <= target_y_max):
+                found = True
+            
+            else:
+                pass
+        
+        t.left(90)
+        
+        for k in range(100):
+            t.forward(1)
+            
+            if (t.xcor() >= target_x_min and t.xcor() <= target_x_max) and (t.ycor() >= target_y_min and t.ycor() <= target_y_max):
+                found = True
+            
+            else:
+                pass
+            
+        t.left(90)
+    
+    logger.critical("AREA SEARCHED")
+    return found
+
+logger = logging.getLogger("CALROV Yaz Autonomus Mission")
+
+logger.critical("MISSION STARTED")
+logger.critical("Mission Area Is Creating And Setting.")
 
 t = turtle.Turtle()
 t.shape('turtle')
@@ -9,7 +48,7 @@ form.setup(800.0, 600.0)
 form.title("AUTONOMUS MISSION")
 form.bgcolor("lightblue")
 
-start_position = (-200, 150)
+start_position = (-150, 150)
 target_position = (100, -150)
 target_radius = 20
 target_depth = -200
@@ -28,53 +67,51 @@ t.color(0, 0, 0)
 t.up()
 t.goto(start_position[0], start_position[1])
 
+logger.critical("Mission Area Created And Setted.")
+logger.critical("Mission Is Starting.")
+
+t.speed(1)
+
 # Dive
 
-t.goto(0, target_depth)
+start = start_position[0]
+logger.critical("DIVE STARTING")
+t.goto(start, target_depth)
+logger.critical("DIVE FINISHED")
 
 # Search
-
-found = False
 
 target_x_max = target_position[0] + target_radius
 target_x_min = target_position[0] - target_radius 
 target_y_max = target_position[1] + target_radius
 target_y_min = target_position[1] - target_radius
 
-for i in range(2):
-    for j in range(100):
-        t.forward(1)
-        
-        if j >= target_x_min or j <= target_x_max:
-            found = True
-            
-        else:
-            pass
-
-    t.left(90)
-
-    for k in range(100):
-        t.forward(1)
-        
-        if k >= target_y_min or k <= target_y_max:
-            found = True
-            
-        else:
-            pass
-
-    t.left(90)
+found = search(start)
 
 # When target found
 
+while found == False:
+    logger.critical("TARGET NOT FOUND")
+    t.goto(-100, target_depth)
+    start += 50
+    found = search(start)
+    
 if found == True:
+    logger.critical("TARGET FOUND")
     t.goto(finish_circle_position[0], finish_circle_position[1])
     t.down()
     t.circle(40)
     t.up()
-
+    
 # Go to the start point
+
+logger.critical("COMING BACK TO THE START POSITION")
 
 t.goto(start_position[0], start_position[1])
 t.left(90)
+
+logger.critical("MISSION FINISHED")
+turtle.delay(3000)
+turtle.bye()
 
 form.mainloop()
